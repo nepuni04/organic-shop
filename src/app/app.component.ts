da71125d7238907088f269d9bd046f731c763c1f
admin <admin>
2017-12-19 16:26:07 +0530

    Signup with email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
-import { Component, OnDestroy } from '@angular/core';
-import { UserService } from './shared/services/user.service';
-import { AuthService } from './shared/services/auth.service';
-import { Subscription } from 'rxjs/Subscription';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   title = 'O-Shop';
-  private subscription: Subscription;
 
-  constructor(private userService: UserService, private auth: AuthService) {
-    this.subscription = this.auth.user$.subscribe((user) => {
-      // if(user) {
-      //   this.userService.save(user)
-      // }
-    })
-  }
-  
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-  
+  constructor() {}
+
 }
